fa636f36806f5f8110e44a71a4b2743f50f4b0fd
dependable-network-8033 <sabrinadimichele1992@gmail.com>
2019-03-03 21:30:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -42,33 +42,7 @@ function ternaryTeenager(age) {
  return ((age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager");
 }
 
- /*function switchAge(age) {
-    case age = 13:
-      return"You are a teenager!";
-      break;
-    case age = 14:
-      return"You are a teenager!";
-      break;
-    case age = 15:
-      return"You are a teenager!";
-      break;
-    case age = 16:
-      return"You are a teenager!";
-      break;
-    case age = 17:
-      return "You are a teenager!";
-      break;
-    case age = 18:
-      return "You are a teenager!";
-      break;
-    case age 19:
-      return "You are a teenager!";
-      break;
-      default:
-        return "You have an age";
-
-  }
-}*/
+ 
 
 function switchAge(age) {
    switch (age) {
