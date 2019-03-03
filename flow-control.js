48d347d14e92debd039008b3d1ac7c8d9a03fb02
dependable-network-8033 <sabrinadimichele1992@gmail.com>
2019-03-03 19:47:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,9 +8,9 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    console.log("You are a teenager!");
+    return "You are a teenager!";
   } else {
-    console.log("You are not a teenager!");
+    return "You are not a teenager!";
   }
 
 }
