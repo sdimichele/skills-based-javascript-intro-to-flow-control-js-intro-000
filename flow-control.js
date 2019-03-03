d866d7041d8587bee47f9fd8156fb080cdc95177
dependable-network-8033 <sabrinadimichele1992@gmail.com>
2019-03-03 20:44:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,18 +25,18 @@ function teenager(age) {
   }
 
 }
-/*
+
 function ageChecker(age) {
   if (age >= 13 && age <= 19 ){
     return "You are a teenager!";
   } else if (age <= 12){
-      return "You are a kid!";
+      return "You are a kid";
     } else {
     return "You are a grownup";
       }
 
 }
-
+ /*
 function ternaryTeenager(age) {
 return  age >= 13 && age <= 19 ? return "You are a teenager" : return"You are not teenager"
 
